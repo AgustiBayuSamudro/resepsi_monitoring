bce19280278231aca29ab6568fcaca0a70750b49
agustibayu <agustibayu@refactory.id>
2026-07-09 21:09:46 +0700

    setup projects resepsi

---
+++ src/main/java/com/agustibayusamudro/Main.java
@@ -0,0 +1,21 @@
+package com.agustibayusamudro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/com/agustibayusamudro/view/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("SISTEM RESEPSI");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
